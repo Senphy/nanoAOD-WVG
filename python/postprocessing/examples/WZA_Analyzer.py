83934549545310d603fb7be4b9f2ca97eae16e25
Sen <sdeng@cern.ch>
2020-04-24 19:32:50 +0800

    update WZA_Analyzer

--- python/postprocessing/examples/WZA_Analyzer.py
+++ python/postprocessing/examples/WZA_Analyzer.py
@@ -8,15 +8,19 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 from importlib import import_module
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 
+from PhysicsTools.NanoAODTools.postprocessing.tools import deltaR
 
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.countHistogramsModule import countHistogramsProducer
 
+test=0
 MET_pass = 0
 photon_pass = 0
 electron_pass = 0
 muon_pass = 0
 dilepton_pass = 0
+btagjet_cut = 0
 class WZAAnalysis(Module):
     def __init__(self):
         pass
@@ -34,6 +38,7 @@ class WZAAnalysis(Module):
         self.out.branch("electron_phi",  "F")
         self.out.branch("muon_phi",  "F")
         self.out.branch("dilepton_mass",  "F")
+        self.out.branch("Generator_weight","F")
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
@@ -43,19 +48,23 @@ class WZAAnalysis(Module):
         electrons = Collection(event, "Electron")
         muons = Collection(event, "Muon")
         photons = Collection(event, "Photon")
-        # jets = Collection(event, "Jet")
-        # Jet_select = ROOT.TLorentzVector()
+        jets = Collection(event, "Jet")
+        jet_select = [] 
         dileptonp4 = ROOT.TLorentzVector()
         photon_select = []
         electron_select = []
         muon_select = [] 
+        # Record the pass numbers for each cut. Noticed that for efficiency, those who can't pass the MET cut may not be counted because it will pass to next event directly.
 
-        dilepton = False
+        # dilepton = False
+        emumu_dilepton = False  #emumu channal selection
         global MET_pass
         global photon_pass
         global electron_pass
         global muon_pass
         global dilepton_pass
+        global btagjet_cut
+        global test
 
         # selection on MET. Pass to next event directly if fail.
         if  event.MET_pt>20:
@@ -66,50 +75,62 @@ class WZAAnalysis(Module):
 
         # selection on photons
         for i in range(0,len(photons)):
-            if  photons[i].pt>30  and  abs(photons[i].eta)<1.44  and  photons[i].cutBased>=3: #pt eta ID cut
+            if  photons[i].pt>20  and  abs(photons[i].eta)<2.5  and  photons[i].cutBased>=3: 
                 photon_select.append(i)
-
-        # selection on electrons
-        for i in range(0,len(electrons)):
-            if  electrons[i].pt>30  and  abs(electrons[i].eta)<2.5  and  electrons[i].cutBased>=3: #pt eta ID cut
-                electron_select.append(i)
-            if i != len(electrons) and dilepton == False:
-                for j in range(i+1,len(electrons)):
-                    if electrons[i].pdgId == -electrons[j].pdgId:
-                        dileptonmass = -1.0
-                        dileptonmass = (electrons[i].p4()+electrons[j].p4()).M()
-                        if dileptonmass >= 60 and dileptonmass <= 120:
-                            dilepton = True                                                      #dilepton selection
-                            self.out.fillBranch("dilepton_mass",dileptonmass)
-                            break
-
-        #selection on muons
-        for i in range(0,len(muons)):
-            if  muons[i].pt>25  and  abs(muons[i].eta)<2.1:  #or  muons[i].cutBased<=3:             #pt eta ID cut
-                muon_select.append(i)
-            if i != len(muons) and dilepton == False:
-                for j in range(i+1,len(muons)):
-                    if muons[i].pdgId == -muons[j].pdgId:
-                        dileptonmass = -1.0
-                        dileptonmass = (muons[i].p4()+muons[j].p4()).M()
-                        if dileptonmass >= 60 and dileptonmass <= 120:
-                            dilepton = True                                                      #dilepton selection
-                            self.out.fillBranch("dilepton_mass",dileptonmass)
-                            break
-                        
-        # Record the pass numbers for each cut. Noticed that for efficiency, those who can't pass the MET cut may not be counted because it will pass to next event directly.
-        if dilepton == False: 
-            return False                        #reject event if there are no di-leptons pass the selection in the event 
-        else:
-            dilepton_pass += 1
         if len(photon_select)==0:
             return False                        #reject event if there is no photon selected in the event
         else:
             photon_pass += 1
+
+        # selection on electrons
+        for i in range(0,len(electrons)):
+            if  electrons[i].pt>20  and  abs(electrons[i].eta)<2.5  and  electrons[i].cutBased>=3:
+                electron_pass += 1
+                electron_select.append(i)
+
+        #selection on muons
+        for i in range(0,len(muons)):
+            if  muons[i].pt>20  and  abs(muons[i].eta)<2.5:  #or  muons[i].cutBased<=3:            
+                muon_pass += 1
+                muon_select.append(i)
+        
+        #dilepton mass
+        if  len(muon_select)==2  and len(electron_select)==1:  #begin with emumu channel 
+            if muons[muon_select[0]].pdgId == -muons[muon_select[1]].pdgId:
+                dileptonmass = -1.0
+                dileptonmass = (muons[muon_select[0]].p4()+muons[muon_select[1]].p4()).M()
+                # if dileptonmass >= 60 and dileptonmass <= 120:
+                print "a=",photon_select, "e=",electron_select, "mu=",muon_select
+                emumu_dilepton = True
+
+        if emumu_dilepton == False: 
+            return False                        #reject event if there are no di-leptons pass the selection in the event 
+        else:
+            dilepton_pass += 1
+
+        # selection on b-tag jet
+        for i in range(0,len(jets)): 
+            # if jets[i].btagCMVA > 0.4432:  # cMVAv2M
+            if jets[i].btagCSVV2 > 0.8484:  # CSVv2M
+            # if jets[i].btagDeepB > 0.6324:  # DeepCSVM
+                if jets[i].pt>30:
+                    continue
+                for j in range(0,len(photon_select)):
+                    if deltaR(jets[i].eta,jets[i].phi,photons[photon_select[j]].eta,photons[photon_select[j]].phi) > 0.3:
+                        btagjet_cut +=1
+                        return False
+                for j in range(0,len(electron_select)):
+                    if deltaR(jets[i].eta,jets[i].phi,electrons[electron_select[j]].eta,electrons[electron_select[j]].phi) > 0.3:
+                        btagjet_cut +=1
+                        return False
+                for j in range(0,len(muon_select)):
+                    if deltaR(jets[i].eta,jets[i].phi,muons[muon_select[j]].eta,muons[muon_select[j]].phi) > 0.3:
+                        btagjet_cut +=1
+                        return False
+
         if len(electron_select)==0 and len(muon_select)==0:      #reject event if there is no lepton selected in the event
             return False
-        elif len(electron_select)!=0: electron_pass += 1
-        elif len(muon_select)!=0: muon_pass += 1
+
 
         for i in range(0,len(photon_select)):
             self.out.fillBranch("photon_pt",photons[photon_select[i]].pt)
@@ -124,13 +145,18 @@ class WZAAnalysis(Module):
             self.out.fillBranch("muon_eta",muons[muon_select[i]].eta)
             self.out.fillBranch("muon_phi",muons[muon_select[i]].phi)
         self.out.fillBranch("event",event.event)
-
+        self.out.fillBranch("dilepton_mass",dileptonmass)
+        self.out.fillBranch("Generator_weight",event.Generator_weight)
         return True
 
 # files=["root://cms-xrd-global.cern.ch//store/mc/RunIISummer16NanoAODv6/WZG_TuneCUETP8M1_13TeV-amcatnlo-pythia8/NANOAODSIM/PUMoriond17_Nano25Oct2019_102X_mcRun2_asymptotic_v7-v1/260000/EE33CA79-B0A1-1145-A457-FE7B7C1A03BC.root"]
-files=["root://cms-xrd-global.cern.ch//store/mc/RunIISummer16NanoAODv6/WZG_TuneCUETP8M1_13TeV-amcatnlo-pythia8/NANOAODSIM/PUMoriond17_Nano25Oct2019_102X_mcRun2_asymptotic_v7-v1/260000/356CFA55-E91B-1940-ACFC-FE3E769A44D5.root"]
+# files=["root://cms-xrd-global.cern.ch//store/mc/RunIISummer16NanoAODv6/WZG_TuneCUETP8M1_13TeV-amcatnlo-pythia8/NANOAODSIM/PUMoriond17_Nano25Oct2019_102X_mcRun2_asymptotic_v7-v1/260000/356CFA55-E91B-1940-ACFC-FE3E769A44D5.root"]
+files=["/afs/cern.ch/work/s/sdeng/config_file/bwcutoff_15_test_submit/SMP-RunIISummer16NanoAODv6-00310.root"]
+# files=["/afs/cern.ch/work/s/sdeng/config_file/bwcutoff_30_test_submit/SMP-RunIISummer16NanoAODv6-00310.root"]
 # files=["root://cms-xrd-global.cern.ch//store/mc/RunIISummer16NanoAODv4/TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6_ext1-v1/260000/FA76270A-417C-174F-B403-8FE5E5A3EFE4.root"]
-p=PostProcessor(".",files,branchsel="input_branch_sel.txt",modules=[WZAAnalysis()],provenance=True,outputbranchsel="output_branch_sel.txt")
+# files=["root://cms-xrd-global.cern.ch//store/mc/RunIISummer16NanoAODv4/ttZJets_13TeV_madgraphMLM/NANOAODSIM/Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/280000/AF15D61D-C169-1F49-B287-DCF0B7F44B8B.root"]
+# files=["root://cms-xrd-global.cern.ch//store/mc/RunIISummer16NanoAODv6/tZq_ll_4f_13TeV-amcatnlo-pythia8/NANOAODSIM/PUMoriond17_Nano25Oct2019_102X_mcRun2_asymptotic_v7_ext1-v1/280000/A85C5B62-D4C8-6845-B005-C0CE9B0AB4EA.root"]
+p=PostProcessor(".",files,branchsel="input_branch_sel.txt",modules=[countHistogramsProducer(),WZAAnalysis()],provenance=True,outputbranchsel="output_branch_sel.txt")
 p.run()
 
 print "MET_pass","\t","=","\t",MET_pass
@@ -138,3 +164,4 @@ print "photon_pass","\t","=","\t",photon_pass
 print "electron_pass","\t","=","\t",electron_pass
 print "muon_pass","\t","=","\t",muon_pass 
 print "dilepton_pass","\t","=","\t",dilepton_pass
+print "btagjet_cut","\t","=","\t",btagjet_cut
