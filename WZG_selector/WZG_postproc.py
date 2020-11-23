baec2417e6db164fb68b81c7be56ef1951ef931e
sen <476025379@qq.com>
2020-11-23 12:25:53 +0800

    Update WZG_postproc.py

--- WZG_selector/WZG_postproc.py
+++ WZG_selector/WZG_postproc.py
@@ -47,8 +47,9 @@ if args.file == '':
     elif args.name == 'ttZJets':
         if args.year == '2016': dataset = "/ttZJets_13TeV_madgraphMLM-pythia8/RunIISummer16NanoAODv7-PUMoriond17_Nano02Apr2020_102X_mcRun2_asymptotic_v8-v1/NANOAODSIM"
     else:
-        print "unknown dataset name"
-        sys.exit(0)
+        if args.mode = 'local':
+            print "unknown dataset name"
+            sys.exit(0)
 
 else:
 
