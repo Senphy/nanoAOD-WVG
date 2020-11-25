597bc7805e9b7cbdb97e7d30cde9057d930a4f3f
sen <476025379@qq.com>
2020-11-25 15:15:07 +0800

    Update DAS_filesearch.py

--- WZG_selector/DAS_filesearch.py
+++ WZG_selector/DAS_filesearch.py
@@ -6,14 +6,44 @@ import optparse
 
 def getFilePath(dataset, name):
 
+    print "Searching for LFN"
     os.system("/cvmfs/cms.cern.ch/common/dasgoclient --query=\"file dataset="+dataset+"\" -limit=0 > filepath_"+name+".txt")
+    print "LFN prepared\n"
+
 
     with open ("filepath_"+name+".txt") as f:
         lines = f.readlines()
         f.close()
+
+
     with open ("filepath_"+name+".txt","w+") as f:
         for line in lines:
-            # f.write("root://cms-xrd-global.cern.ch//eos/cms"+line)
-            f.write("root://cms-xrd-global.cern.ch/"+line)
+            line = line.rstrip('\n') 
+
+
+            print "Searching for valid site"
+            os.system("cmsRun ${CMSSW_BASE}/src/PhysicsTools/NanoAODTools/WZG_selector/test_ValidSite_cfg.py inputFiles=\""+line+"\" > test_ValidSite.log 2>&1")
+
+
+            with open ("test_ValidSite.log","r") as test:
+                test_lines = test.read()
+                # print test_lines
+                if 'Successfully' in test_lines:
+                    valid_site = test_lines.split('Successfully opened file ')[1].split('/store/mc')[0]
+                    print "valid site for ", line, ": ", valid_site, "\n"
+                    f.write(valid_site+line+"\n")
+
+
+                else:
+                    print "no accessable site for "+line
+                    os.remove("test_ValidSite.log")
+                    sys.exit(0)
+                test.close()
+
+
+            os.remove("test_ValidSite.log")
+
+
+        # f.write("root://cms-xrd-global.cern.ch//eos/cms"+line)
         f.close()
 
