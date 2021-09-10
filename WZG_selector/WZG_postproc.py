bd3c8df0b0c4dd7d629af1d6364d6cc6df762dd9
sen <476025379@qq.com>
2021-09-10 16:11:31 +0800

    Update WZG_postproc.py

--- WZG_selector/WZG_postproc.py
+++ WZG_selector/WZG_postproc.py
@@ -8,6 +8,11 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.common.countHistogramsModu
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2       import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.PrefireCorr import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.eleRECOSFProducer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.eleIDSFProducer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.muonScaleResProducer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.muonIDISOSFProducer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.WZG_Module import *
 
 from WZG_Module import * 
 
@@ -15,6 +20,9 @@ import argparse
 import re
 import optparse
 
+PrefCorrUL16_preVFP = lambda : PrefCorr(jetroot="L1PrefiringMaps.root", jetmapname="L1prefiring_jetptvseta_UL2016preVFP", photonroot="L1PrefiringMaps.root", photonmapname="L1prefiring_photonptvseta_UL2016preVFP", branchnames=["PrefireWeight","PrefireWeight_Up", "PrefireWeight_Down"])
+PrefCorrUL16_postVFP = lambda : PrefCorr(jetroot="L1PrefiringMaps.root", jetmapname="L1prefiring_jetptvseta_UL2016postVFP", photonroot="L1PrefiringMaps.root", photonmapname="L1prefiring_photonptvseta_UL2016postVFP", branchnames=["PrefireWeight","PrefireWeight_Up", "PrefireWeight_Down"])
+PrefCorrUL17 = lambda : PrefCorr(jetroot="L1PrefiringMaps.root", jetmapname="L1prefiring_jetptvseta_UL2017BtoF", photonroot="L1PrefiringMaps.root", photonmapname="L1prefiring_photonptvseta_UL2017BtoF", branchnames=["PrefireWeight","PrefireWeight_Up", "PrefireWeight_Down"])
 
 parser = argparse.ArgumentParser(description='baseline selection')
 parser.add_argument('-f', dest='file', default='', help='File input. In local mode it will be the filepath. In condor mode it will be the dataset name')
@@ -31,26 +39,29 @@ args = parser.parse_args()
 if args.isdata:
     if args.year == '2018':
         jetmetCorrector = createJMECorrector(isMC=False, dataYear="UL2018", runPeriod=args.period, metBranchName="MET")
+        Modules = [muonScaleRes2018(),jetmetCorrector(),WZG_select_Module()]
     if args.year == '2017':
         jetmetCorrector = createJMECorrector(isMC=False, dataYear="UL2017", runPeriod=args.period, metBranchName="MET")
+        Modules = [muonScaleRes2017(),jetmetCorrector(),WZG_select_Module()]
     if args.year == '2016':
         jetmetCorrector = createJMECorrector(isMC=False, dataYear="UL2016", runPeriod=args.period, metBranchName="MET")
+        Modules = [muonScaleRes2016b(),jetmetCorrector(),WZG_select_Module()]
     if args.year == '2016_PreVFP':
         jetmetCorrector = createJMECorrector(isMC=False, dataYear="UL2016_PreVFP", runPeriod=args.period, metBranchName="MET")
-    Modules = [countHistogramsProducer(),jetmetCorrector(),WZG_select_Module()]
+        Modules = [muonScaleRes2016a(),jetmetCorrector(),WZG_select_Module()]
 else:
     if args.year == '2018':
         jetmetCorrector = createJMECorrector(isMC=True, dataYear="UL2018", jesUncert="Total", metBranchName="MET", splitJER=False, applyHEMfix=True)
-        Modules = [countHistogramsProducer(),jetmetCorrector(),WZG_select_Module(),puWeight_2018()]
+        Modules = [countHistogramsProducer(),puAutoWeight_2018(),muonIDISOSF2018(),muonScaleRes2018(),eleRECOSF2018(),eleIDSF2018(),jetmetCorrector(),WZG_select_Module()]
     if args.year == '2017':
         jetmetCorrector = createJMECorrector(isMC=True, dataYear="UL2017", jesUncert="Total", metBranchName="MET", splitJER=False)
-        Modules = [countHistogramsProducer(),PrefCorr(),jetmetCorrector(),WZG_select_Module(),puWeight_2017()]
+        Modules = [countHistogramsProducer(),puAutoWeight_2017(),PrefCorrUL17(),muonIDISOSF2017(),muonScaleRes2017(),eleRECOSF2017(),eleIDSF2017(),jetmetCorrector(),WZG_select_Module()]
     if args.year == '2016':
         jetmetCorrector = createJMECorrector(isMC=True, dataYear="UL2016", jesUncert="Total", metBranchName="MET", splitJER=False)
-        Modules = [countHistogramsProducer(),PrefCorr(),jetmetCorrector(),WZG_select_Module(),puWeight_2016()]
+        Modules = [countHistogramsProducer(),puAutoWeight_2016(),PrefCorrUL16_postVFP(),muonIDISOSF2016(),muonScaleRes2016b(),eleRECOSF2016(),eleIDSF2016(),jetmetCorrector(),WZG_select_Module()]
     if args.year == '2016_PreVFP':
         jetmetCorrector = createJMECorrector(isMC=True, dataYear="UL2016_PreVFP", jesUncert="Total", metBranchName="MET", splitJER=False)
-        Modules = [countHistogramsProducer(),PrefCorr(),jetmetCorrector(),WZG_select_Module(),puWeight_2016()]
+        Modules = [countHistogramsProducer(),puAutoWeight_2016(),PrefCorrUL16_preVFP(),muonIDISOSF2016(),muonScaleRes2016a(),eleRECOSF2016(),eleIDSF2016(),jetmetCorrector(),WZG_select_Module()]
 
 if args.file:
 
