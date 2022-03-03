3379b30e515b217847977b80a0148b37c0853e3f
sen <476025379@qq.com>
2022-03-03 15:28:21 +0800

    Update ALP related

---
+++ condor_for_mc/ALP_production/batch_submit.py
@@ -0,0 +1,44 @@
+import os,sys
+import shutil
+import subprocess
+
+def ALP_batch_condor():
+    MassesALP = [50, 90, 100, 110, 160, 200, 300, 400]
+    year = 2018
+    eospath = '/eos/user/s/sdeng/WZG_analysis/ALP/'
+    initial_path = os.getcwd()
+
+    for mass in MassesALP:
+        Sample_Name = "ALP_lvlla_4f_LO_m%s" %(str(mass))
+        if os.path.isdir(Sample_Name):
+            shutil.rmtree(Sample_Name)
+        os.makedirs(Sample_Name+'/log')
+        if not os.path.isdir(eospath + str(year) + '/' + Sample_Name):
+            os.makedirs(eospath + str(year) + '/' + Sample_Name)
+
+
+        shutil.copyfile('cmssw-cc7', Sample_Name+'/cmssw-cc7')
+        shutil.copyfile('filepath_Neutrino_E-10_gun_%s.txt' %(str(year)), Sample_Name+'/filepath_Neutrino_E-10_gun_%s.txt' %(str(year)))
+        shutil.copyfile('randomizeSeeds.py', Sample_Name+'/randomizeSeeds.py')
+        shutil.copyfile('slc7_active_ALP_production_%s_v9.sh' %(str(year)), Sample_Name+'/slc7_active_ALP_m%s_production_%s_v9.sh' %(str(mass), str(year)))
+        shutil.copyfile('submit_ALP_production_%s_v9.jdl' %(str(year)), Sample_Name+'/submit_ALP_m%s_production_%s_v9.jdl' %(str(mass), str(year)))
+        shutil.copyfile('wrapper_ALP_production_%s_v9.sh' %(str(year)), Sample_Name+'/wrapper_ALP_m%s_production_%s_v9.sh' %(str(mass), str(year)))
+        shutil.copyfile(eospath+'/ALP_lvlla_4f_LO_m%s_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz' %(str(mass)), Sample_Name+'/ALP_lvlla_4f_LO_m%s_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz' %(str(mass)))
+
+        os.popen('sed -i "s/WRAPPER.sh/wrapper_ALP_m%s_production_%s_v9.sh/g" %s/submit_ALP_m%s_production_%s_v9.jdl' %(str(mass), str(year), Sample_Name, str(mass), str(year)))
+        os.popen('sed -i "s/EXECUTABLE.sh/slc7_active_ALP_m%s_production_%s_v9.sh/g" %s/submit_ALP_m%s_production_%s_v9.jdl' %(str(mass), str(year), Sample_Name, str(mass), str(year)))
+        os.popen('sed -i "s/ROOTNAME/ALP_lvlla_4f_LO_m%s/g" %s/submit_ALP_m%s_production_%s_v9.jdl' %(str(mass), Sample_Name, str(mass), str(year)))
+        os.popen('sed -i "s/YEAR/%s/g" %s/submit_ALP_m%s_production_%s_v9.jdl' %(str(year), Sample_Name, str(mass), str(year)))
+        os.popen('sed -i "s/TARBALLNAME/ALP_lvlla_4f_LO_m%s_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz/g" %s/submit_ALP_m%s_production_%s_v9.jdl' %(str(mass), Sample_Name, str(mass), str(year)))
+        os.popen('sed -i "s/TARBALLNAME/ALP_lvlla_4f_LO_m%s_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz/g" %s/wrapper_ALP_m%s_production_%s_v9.sh' %(str(mass), Sample_Name, str(mass), str(year)))
+        os.popen('sed -i "s/WRAPPER.sh/wrapper_ALP_m%s_production_%s_v9.sh/g" %s/slc7_active_ALP_m%s_production_%s_v9.sh' %(str(mass), str(year), Sample_Name, str(mass), str(year)))
+        
+        # Have some potential bugs here! *FIXME*
+        os.chdir(Sample_Name)
+        subprocess.call('condor_submit submit_ALP_m%s_production_%s_v9.jdl' %(str(mass), str(year)), shell=True)
+        os.chdir(initial_path)
+
+    pass
+
+if __name__ == '__main__':
+    ALP_batch_condor()
---
+++ condor_for_mc/ALP_production/slc7_active_ALP_production_2018_v9.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+exit_on_error() {
+    result=$1
+    code=$2
+    message=$3
+
+    if [ $1 != 0 ]; then
+        echo $3
+        exit $2
+    fi
+}
+
+# set up cmssw
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+
+cat WRAPPER.sh | bash cmssw-cc7
